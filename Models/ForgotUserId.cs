45fca0ea3f964b373aecd2098af36b58df15a9b2
Geetanjali Sharma <41438124+sharmageetanjali163@users.noreply.github.com>
2021-05-03 13:39:49 +0530

    Model Classes Updated
    ForgotUserId.cs
    ForgotPassword.cs
    ResetPassword.cs

---
+++ Models/ForgotUserId.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class ForgotUserId
+    {
+        [Display(Name = "Contact Number")]
+        [Required(ErrorMessage = "Contact Number Required")]
+        [MinLength(10, ErrorMessage = "Minimum 10 digits")]
+        [DataType(DataType.PhoneNumber)]
+        public long ContactNumber { get; set; }
+
+        [Display(Name = "Favourite Song")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Answer Required")]
+        public string Ques1 { get; set; }
+
+        [Display(Name = "Favourite Color")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Answer Required")]
+        public string Ques2 { get; set; }
+
+        [Display(Name = "Favourite Pet")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Answer Required")]
+        public string Ques3 { get; set; }
+    }
+}
---
+++ Models/ResetPassword.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class ResetPassword
+    {
+        [Display(Name = "Enter New Password")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "No Password Entered")]
+        [DataType(DataType.Password)]
+        [MinLength(6, ErrorMessage = "Minimum Length shoule be 6 characters")]
+        public string NewPassword { get; set; }
+    }
+}
